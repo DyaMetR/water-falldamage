8b1f0e038745f977a07305a76445cfec83ec3bde
Alberto Blanco Celdrán <flaberto.abc@gmail.com>
2019-06-09 21:26:10 +0200

    Added a mode to calculate damage only with air speed. Fixes.
    Fixed water fall damage taking suit armor energy.

--- lua/autorun/waterfalldamage.lua
+++ lua/autorun/waterfalldamage.lua
@@ -1,18 +1,17 @@
 --[[-----------------
+ WATER FALLING DAMAGE
+	   Version 1.1
+	    09/12/16
 
-	WATER FALL DAMAGE
-	by DyaMetR
-	
-	Version 1
-	28/12/16
-
+by DyaMetR
 ]]-------------------
 
 if SERVER then
 
-	CreateConVar( "wfd_enabled", "1", {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Water fall damage toggle" ) 
-	CreateConVar( "wfd_mul", "1", {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Water fall damage multiplier" ) 
-	
+	local enabled = CreateConVar( "wfd_enabled", "1", {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_NEVER_AS_STRING}, "Water fall damage toggle" );
+	local mul = CreateConVar( "wfd_mul", "1", {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_NEVER_AS_STRING}, "Water fall damage multiplier" );
+	local airSpeedOnly = CreateConVar( "wfd_speed_only", "0", {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_NEVER_AS_STRING}, "Calculate damage only with air speed" );
+
 	local function Spawn(ply)
 		ply.WaterFallDamage = {
 			TakesDamage = false,
@@ -21,60 +20,69 @@ if SERVER then
 		}
 	end
 	hook.Add("PlayerSpawn", "wfd_spawn", Spawn)
-	
+
 	local function Think(ply)
 
-		if tonumber(GetConVar("wfd_enabled"):GetString()) <= 0 then return false end
-		
+		if enabled:GetInt() <= 0 then return end
+
 		if ply:WaterLevel() > 0 then
-		
+
 			if !ply.WaterFallDamage.InWater then
-				if ply.WaterFallDamage.AirSpeed >= 525 and ply.WaterFallDamage.TakesDamage and ply:GetMoveType() != MOVETYPE_NOCLIP then
+				if ply.WaterFallDamage.AirSpeed >= 525 and (ply.WaterFallDamage.TakesDamage or airSpeedOnly:GetInt() >= 1) and ply:GetMoveType() != MOVETYPE_NOCLIP then
 					local var = math.floor((ply.WaterFallDamage.AirSpeed - 460)/485) -- 420/485
-					local mul = math.Clamp(var,0,var)
-					ply:TakeDamage(mul*20*tonumber(GetConVar("wfd_mul"):GetString()))
-					ply:ViewPunch(Angle(0, 0, 20*math.Clamp(mul,0,2)))
+					local damage = math.Clamp(var,0,var)
+
+					-- Take damage
+					local dmgInfo = DamageInfo();
+					dmgInfo:SetDamage(damage * 20 * mul:GetFloat());
+					dmgInfo:SetAttacker(game.GetWorld());
+					dmgInfo:SetInflictor(game.GetWorld());
+					dmgInfo:SetDamageType(DMG_FALL);
+					dmgInfo:SetDamageForce(Vector(0, 0, 0));
+					ply:TakeDamageInfo(dmgInfo);
+
+					ply:ViewPunch(Angle(0, 0, 20*math.Clamp(damage,0,2)))
 					ply:EmitSound(Sound("physics/body/body_medium_impact_hard"..math.random(1,6)..".wav"))
 				end
 				ply.WaterFallDamage.InWater = true
 				ply.WaterFallDamage.TakesDamage = false
 			end
-			
+
 		else
-		
+
 			if ply.WaterFallDamage.InWater then
 				ply.WaterFallDamage.InWater = false
 			end
-			
+
 			if ply.WaterFallDamage.AirSpeed != ply:GetVelocity():Length() then
 				ply.WaterFallDamage.AirSpeed = ply:GetVelocity():Length()
 			end
-			
+
 			if ply:OnGround() or ply:GetMoveType() == MOVETYPE_NOCLIP then
-			
+
 				if ply.WaterFallDamage.TakesDamage then
 					ply.WaterFallDamage.TakesDamage = false
 				end
-				
+
 			else
-			
+
 				if !ply.WaterFallDamage.TakesDamage then
-				
+
 					local trace = util.TraceLine( {
 						start = ply:GetPos(),
 						endpos = ply:GetPos() + Vector(0,0,-300),
 						mask = bit.bor( MASK_WATER, MASK_SOLID ),
 						filter = ply
 					} )
-					
+
 					if !trace.Hit then
 						ply.WaterFallDamage.TakesDamage = true
 					end
-					
+
 				end
-			
+
 			end
-			
+
 		end
 
 	end
@@ -93,17 +101,23 @@ if CLIENT then
 			Command = "wfd_enabled",
 			}
 		)
-		
+
 		Panel:AddControl( "Textbox", {
 				Label = "Damage multiplier",
 				Command = "wfd_mul",
 			}
 		)
-	
+
+		Panel:AddControl( "CheckBox", {
+				Label = "Calculate damage only with air speed",
+				Command = "wfd_speed_only",
+			}
+		)
+
 	end
 
 	local function createthemenu()
-		spawnmenu.AddToolMenuOption( "Options", "DyaMetR", "wfd", "Water Fall Damage", "", "", menu ) 
+		spawnmenu.AddToolMenuOption( "Utilities", "DyaMetR", "wfd", "Water Fall Damage", "", "", menu )
 	end
 	hook.Add( "PopulateToolMenu", "wfd_menu", createthemenu )
 end
